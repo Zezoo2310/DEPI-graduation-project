658786ea5158c22287b60b9da2b941ef0cba3738
Zezoo2310 <abdalazezmerzgeid@gmail.com>
2026-07-01 20:24:56 +0300

    Delete Video_Analysis directory

--- Video_Analysis/verify_tf.py
+++
@@ -1,8 +0,0 @@
-import sys
-import traceback
-try:
-    import tensorflow as tf
-    print("TF version:", tf.__version__)
-except Exception as e:
-    print("Exception occurred:")
-    traceback.print_exc()
